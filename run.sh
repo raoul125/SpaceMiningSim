ae7c735f6333416d6c23fe28691d6196522192b8
Sedeck <sedecktema@gmail.com>
2024-08-25 19:53:37 -0700

    saving changes

--- run.sh
+++ run.sh
@@ -3,13 +3,13 @@
 # Determine the operating system
 if [[ "$OSTYPE" == "linux-gnu" ]]; then
   echo "Running on Linux"
-  ./bin/LunarMiningSim
+  ./bin/LunarMiningSim $1 $2 $3
 elif [[ "$OSTYPE" == "darwin" ]]; then
   echo "Running on macOS"
-  ./bin/LunarMiningSim
+  ./bin/LunarMiningSim $1 $2 $3
 elif [[ "$OSTYPE" == "cygwin" || "$OSTYPE" == "msys" ]]; then
   echo "Running on Windows"
-  ./bin/LunarMiningSim.exe
+  ./bin/LunarMiningSim.exe $1 $2 $3
 else
   echo "Unsupported platform"
   exit 1
--- source/LunarMiningSim.cpp
+++ source/LunarMiningSim.cpp
@@ -10,15 +10,43 @@
 
 #include "Simulator/Simulator.h"
 #include <iostream>
+#include <string>
 
-int main()
+int main(int argc, char* argv[])
 {
+    // Check if the correct number of arguments is provided
+    if (argc != 4) {
+        std::cerr << "Usage: LunarMiningSim <num_trucks> <num_mining_sites> <num_unload_stations>" << std::endl;
+        return 1;
+    }
+
+    // Parse the arguments
+    int num_trucks = std::stoi(argv[1]);
+    int num_mining_sites = std::stoi(argv[2]);
+    int num_unload_stations = std::stoi(argv[3]);
+    
+    // Make sure all input are valid
+    if (num_trucks <= 0 || num_mining_sites <= 0 || num_unload_stations <= 0)
+    {
+        std::cerr << "Invalid arguments. All arguments must be positive integers." << std::endl;
+        return 1;
+    }
+
+    // number of sites must be greater than number of trucks and unload_stations
+    if (num_mining_sites < num_trucks || num_unload_stations < num_trucks)
+    {
+        std::cerr << "Invalid arguments. Number of mining sites must be greater than number of trucks and unload stations." << std::endl;
+        return 1;
+    }
+
     Simulator lunar_sim;
-    // Initialize the simulation with 1 truck, 3 mining sites, and 1 unload station
-    lunar_sim.Initialize(1, 2, 2);
+    // Initialize the simulation with the provided parameters
+    lunar_sim.Initialize(num_trucks, num_mining_sites, num_unload_stations);
     lunar_sim.Run();
 
-    std::cout << "Done Running" << std::endl;
+    // ask user to press any key to continue
+    std::cout << "Simulation Complete. Please press any key to exit..." << std::endl;
+    std::cin.get();
 
     return 0;
 }
